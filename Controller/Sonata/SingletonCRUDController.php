433d609e24f7e328d733810835103359af873683
Solène Garda-Krebs <sgardakrebs@umanit.fr>
2022-07-08 10:42:15 +0200

    fix: modified SingletonCRUDController to use both old and new sonata admin version

--- Controller/Sonata/SingletonCRUDController.php
+++ Controller/Sonata/SingletonCRUDController.php
@@ -2,7 +2,6 @@
 
 namespace Umanit\DoctrineSingletonBundle\Controller\Sonata;
 
-use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Tools\Pagination\Paginator;
 use Sonata\AdminBundle\Controller\CRUDController;
 use Umanit\MultiSiteBundle\Utils\SiteAccessesManager;
@@ -17,16 +16,27 @@ class SingletonCRUDController extends CRUDController
      */
     public function listAction()
     {
-        /** @var Paginator $resultList */
+        /** @var Paginator|array $resultList */
         $resultList = $this->admin->getDatagrid()->getResults();
 
-        if ($resultList->count() > 1) {
-            return parent::listAction();
-        }
+        if ($resultList instanceof Paginator) {
+            if ($resultList->count() > 1) {
+                return parent::listAction();
+            }
 
-        if (1 === $resultList->count()) {
-            $result = $resultList->getIterator()->current();
-            return $this->redirect($this->admin->generateObjectUrl('edit', $result));
+            if (1 === $resultList->count()) {
+                $result = $resultList->getIterator()->current();
+                return $this->redirect($this->admin->generateObjectUrl('edit', $result));
+            }
+        } else {
+            $result = $this->admin->getDatagrid()->getResults();
+            if (count($result) > 1) {
+                return parent::listAction();
+            }
+
+            if (count($result) === 1) {
+                return $this->redirect($this->admin->generateObjectUrl('edit', $result[0]));
+            }
         }
 
         return $this->redirect($this->admin->generateUrl('create'));
